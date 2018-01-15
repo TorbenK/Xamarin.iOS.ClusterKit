00f813ca877ab27ada6bb545bf7556b2bf763108
webbercross <geoff_cross@hotmail.com>
2018-01-15 07:43:52 -0800

    added ck xcode project

---
+++ clusterkit/ClusterKit/Core/ClusterKit.h
@@ -0,0 +1,23 @@
+//
+//  ClusterKit.h
+//  ClusterKit
+//
+//  Created by user135166 on 1/12/18.
+//  Copyright © 2018 WebberCross. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ClusterKit.
+FOUNDATION_EXPORT double ClusterKitVersionNumber;
+
+//! Project version string for ClusterKit.
+FOUNDATION_EXPORT const unsigned char ClusterKitVersionString[];
+
+#import <ClusterKit/CKClusterManager.h>
+#import <ClusterKit/CKAnnotationTree.h>
+#import <ClusterKit/CKClusterAlgorithm.h>
+#import <ClusterKit/CKNonHierarchicalDistanceBasedAlgorithm.h>
+#import <ClusterKit/CKGridBasedAlgorithm.h>
+#import <ClusterKit/CKMap.h>
+#import <ClusterKit/CKCluster.h>
